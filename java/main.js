1a2a7d28a3d726ecae7bcceb8f327100274fb040
diesel-007 <vysockijv928@gmail.com>
2022-02-18 14:43:43 +0200

    домашняя работа java-1

---
+++ java/main.js
@@ -0,0 +1,68 @@
+//let number= 100500;
+//console.log(number+200);
+//let someMsg=`maine name is`
+  //  console.log(someMsg);
+//console.log(typeof someMsg);
+//let Pi = 3.14;
+//Pi=123456;
+//console
+//console.log(Pi);
+//{
+//     let b=`world`;
+//     var a=`hello`;
+//     console.log(a);
+//     console.log(b);
+//}
+// console.log(true);
+// console.log(false);
+// let bool1= 5>6;
+// console.log(bool1);
+// console.log(typeof true);
+// let bool2=5===5;
+// console
+// console.log(typeof bool2);
+
+//let num=12345;
+//let numbers=[100,200,300,num];
+//console.log(numbers);
+//console.log(numbers[2]);
+//console.log(numbers[3]);
+//console.log(numbers.length);
+//yyy=12345;
+//console.log(yyy);
+//let arr=[`a`,`b`,`c`,[11,22,33]];
+//console.log(arr[3][2]);
+// let product1={
+//     title:`milk`,
+//     price:`23`,
+//     quantity:2
+// }
+// console.log(product1);
+// console.log(product1.title);
+// console.log(product1.quantity);
+//
+// let product2 = {
+//     title: `beer`,
+//     price: `23`,
+//     quantity:2,
+//     manufacturer: {
+//         title: `chernigivske`, city: `Chernigiv`, zip: 12345,
+//         conservants: [
+//             {id: `c155`, status: 1},
+//             {id: `a10`, status: 2},
+//             {id: `b1`, status: 3}
+//         ]
+//     },
+//     }
+// console.log(product2.title)
+// console.log(product2.manufacturer.title);
+// console.log(product2[`title`]);
+// console.log(product2[`manufacturer`][`title`]);
+// console.log(product2[`manufacturer`]. title);
+// console.log(product2.manufacturer.conservants[2]);
+// console.log(product2.manufacturer.conservants[1].status);
+
+
+console.log(5+10);
+console.log(5-10);
+console.log(5*10);
